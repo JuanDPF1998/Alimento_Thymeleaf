48e206a942925824c6cc37e8277bf34381cf841f
juandanielpf1998@gmail.com <juanDPF1998@gmail.com>
2025-12-25 11:22:34 -0600

    update class and plantillas

--- src/main/java/com/thymeleaft/Repositorio/AlimentosRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.thymeleaft.Repositorio;
-
-import com.thymeleaft.modelo.Alimentos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlimentosRepositorio extends JpaRepository<Alimentos, Long> {
-}
--- src/main/java/com/thymeleaft/Servicio/AlimentosServicio.java
+++
@@ -1,9 +0,0 @@
-package com.thymeleaft.Servicio;
-
-import com.thymeleaft.modelo.Alimentos;
-
-import java.util.List;
-
-public interface AlimentosServicio {
-    public List<Alimentos> listar();
-}
